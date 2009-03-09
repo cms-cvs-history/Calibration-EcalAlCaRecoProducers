ff6772d494fdec3367c742c5cf5fb228f44868c5
cmsbuild <>
2009-03-09 09:00:32 +0000

    This commit was manufactured by cvs2git to create tag 'Calibration- EcalAlCaRecoProducers-V00-17-27'.
    Sprout from master 2009-03-09 09:00:31 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'specify trigger for both 1E31 and 8E29'
    Cherrypick from master 2008-10-30 16:44:38 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'added throw paramter set to False':
        python/ewkHLTFilter_cfi.py
    Delete:
        test/AlCaDoubleElectrons.cfg
        test/AlCaDoubleElectrons_cfg.py
        test/AlCaSingleElectrons.cfg

---
+++ python/ewkHLTFilter_cfi.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+ewkHLTFilter = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+ewkHLTFilter.throw = False 
+
