b96ac828f1f51f7b88f9bdd054f96cbfe8e47628
cmsbuild <>
2009-09-14 15:49:12 +0000

    This commit was manufactured by cvs2git to create tag 'Calibration- EcalAlCaRecoProducers-V00-17-42-10'.
    Sprout from master 2009-04-22 09:45:25 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'Remove duplicate event content, keep only corrected rechits'
    Cherrypick from master 2009-09-14 15:49:09 UTC Vladimir Litvine <sha1-efefdf01597b8d9863f035b5cb6fdad528cd6581@cern.ch> 'MEtoEDMConverter was added to the pi0/eta output':
        python/ALCARECOEcalCalEtaCalib_Output_cff.py
        python/ALCARECOEcalCalPi0Calib_Output_cff.py
        python/alCaIsolatedElectrons_cfi.py
    Cherrypick from master 2009-04-23 13:49:10 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'removed unneeded electronFilter and harmfule isolationSequence from the ALCAIsolElectron sequence':
        python/ALCARECOEcalCalIsolElectron_cff.py
    Cherrypick from master 2009-05-04 19:24:29 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'Removed isolation stuff from eventcontent, isolation variables are now with gsfElectrons':
        python/ALCARECOEcalCalIsolElectron_Output_cff.py

--- python/ALCARECOEcalCalIsolElectron_Output_cff.py
+++ python/ALCARECOEcalCalIsolElectron_Output_cff.py
@@ -5,31 +5,16 @@ OutALCARECOEcalCalElectron = cms.PSet(
         SelectEvents = cms.vstring('pathALCARECOEcalCalElectron')
     ),
     outputCommands = cms.untracked.vstring('drop  *', 
-        'keep  *_electronFilter_*_*', 
+#	'keep recoGsfTracks_*_*_*',
+#	'keep recoGsfTrackExtras_*_*_*',
+	'keep recoGsfElectronCores_*_*_*',
+	'keep recoSuperClusters_*_*_*',
+	'keep *_electronGsfTracks_*_*', 
+        'keep  *_gsfElectrons_*_*', 
         'keep  *_alCaIsolatedElectrons_*_*', 
         'keep recoCaloMETs_met_*_*',
         'keep edmTriggerResults_TriggerResults__*', 
         'keep edmHepMCProduct_*_*_*',                                   
-
-#        'keep *_egammaEcalIsolation_*_*', 
-#        'keep *_egammaEcalRelIsolation_*_*', 
-#        'keep *_egammaElectronSqPtTkIsolation_*_*', 
-#        'keep *_egammaElectronTkIsolation_*_*',
-#        'keep *_egammaElectronTkNumIsolation_*_*', 
-#        'keep *_egammaElectronTkRelIsolation_*_*', 
-#        'keep *_egammaHOETower_*_*', 
-#        'keep *_egammaHOE_*_*', 
-#        'keep *_egammaHcalIsolation_*_*', 
-#        'keep *_egammaTowerIsolation_*_*',
-   
-
-        'keep *_eleIsoFromDepsTk_*_*',                       
-        'keep *_eleIsoFromDepsEcalFromHits_*_*',                       
-        'keep *_eleIsoFromDepsHcalFromTowers_*_*',                       
-        'keep *_eleIsoFromDepsHcalDepth1FromTowers_*_*',                       
-        'keep *_eleIsoFromDepsHcalDepth2FromTowers_*_*',                       
-
-
         'keep *_MEtoEDMConverter_*_*')
 )
 
